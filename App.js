ad72eb56f6c013c30f718fe19a7c530b578dbd16
Luis Leal <luisrrleal@gmail.com>
2024-07-10 20:59:21 -0600

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
+// ADD LOCAL ASSETS
+// const icon = require("./assets/icon.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "white" }}>Tenemos aquí la app</Text>
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Metacritic.svg/1024px-Metacritic.svg.png",
+        }}
+        style={{
+          width: 100,
+          height: 200,
+          resizeMode: "contain",
+        }}
+      />
+      <StatusBar style="light" />
+      <Button title="Pulsa aquí" onPress={() => alert("Hola Mundo")} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    color: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
